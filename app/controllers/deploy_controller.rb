48facdc3a193e15ab533ba2b458af31888cad2a5
Cal Heldenbrand <cal@fbsdata.com>
2015-06-08 10:46:09 -0500

    Update to deploy hook

--- app/controllers/deploy_controller.rb
+++ app/controllers/deploy_controller.rb
@@ -1,5 +1,12 @@
 class DeployController < ApplicationController
-  #protect_from_forgery with: :null_session, only: [:properties]
+  protect_from_forgery with: :null_session, only: [:webhook]
   def webhook
+    # Super cheesy deployment system... for now
+    if params[:ref] == "refs/heads/master" && params[:repository]["name"] == "testcmavendor"
+      logger.info `git pull origin master`
+      logger.info `bundle install`
+      logger.info `touch #{Rails.root}/tmp/restart.txt`
+    end
+    render json: {}
   end
 end
